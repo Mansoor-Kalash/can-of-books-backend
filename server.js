09257e4b5d5e0e9e134db003b8b1490d46988b12
Mansoor-Kalash <mansoor.jow@gmail.com>
2021-08-26 04:32:59 +0300

    coplete add new book

--- server.js
+++ server.js
@@ -11,38 +11,36 @@ const mongoose = require('mongoose');
 
 const App = express();
 App.use(cors());
+App.use(express.json());
 mongoose.connect('mongodb://localhost:27017/Book', {useNewUrlParser: true, useUnifiedTopology: true});
 
 const Book = new mongoose.Schema({
-  img : String,
+  email :String,
   title : String,
   description : String,
   status :String,
-  email :String,
 });
 
 const bookModel = mongoose.model('book', Book);
 function seedDataCollection() {
   const JavaScript = new bookModel({
-    img : 'https://images-na.ssl-images-amazon.com/images/I/91asIC1fRwL.jpg',
+    email :'mansoor.jow@gmail.com',
     title:'Eloquent JavaScript',
     description:'Completely revised and updated, this best-selling introduction to programming in JavaScript focuses on writing real applications. JavaScript lies at the heart of almost every modern web application, from social apps like Twitter to browser-based game frameworks like Phaser and Babylon.',
     status:'good',
-    email :'mansoor.jow@gmail.com',
+    
   })
   const Java = new bookModel({
-    img : 'https://images-na.ssl-images-amazon.com/images/I/51dBjBGQFXS._SX431_BO1,204,203,200_.jpg',
+    email :'mansoor.jow@gmail.com',
     title:'Head First Java',
     description:'Between Moores law and the notion of "Internet time," were constantly being bombarded with more and more information--most of it in the form of disorganized data.',
     status:'good',
-    email :'mansoor.jow@gmail.com',
   })
   const Practice = new bookModel({
-    img :'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQemEUo2qhqhZXvUe8IcVD1ZtQaJgLuTBQ4S4hQZ8kqzEI_ws-Q',
+    email :'mansoor.jow@gmail.com',
     title:'Java Concurrency in Practice',
     description:'I was fortunate indeed to have worked with a fantastic team on the design and implementation of the concurrency features added to the Java platform in Java 5.0 and Java 6. Now this same team provides the best explanation yet of these new features,',
     status:'good',
-    email :'mansoor.jow@gmail.com',
   })
 
 
@@ -56,6 +54,8 @@ function seedDataCollection() {
 // seedDataCollection();
 
 App.get('/book', getFavBook);
+App.post('/addbook', newFavBook);
+
 
 
 function getFavBook(req,res) {
@@ -70,17 +70,51 @@ function getFavBook(req,res) {
       }
   })
 }
+async function newFavBook (req,res){
+console.log('hhhhhhhhhhhh',req.body);
+let { title,description,status,email } = req.body;
+const newBook = new bookModel({
+  email :email,
+  title:title,
+  description:description,
+  status:status,
+})
+
+await newBook.save();
+
+bookModel.find({email:email},function(err,userBook){
+  if(err) {
+      console.log('error in getting the data')
+  } else {
+      console.log(userBook);
+      res.send(userBook);
+  }
+})
+ console.log('fffffffffffffffffffffffffffffffffff');
+
+}
 
 const PORT = process.env.PORT;
 
-App.get('/book', (request, response) => {
+// App.get('/book', (request, response) => {
 
-  // TODO: 
-  // STEP 1: get the jwt from the headers
-  // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
-  // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
-  // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
+//   // TODO: 
+//   // STEP 1: get the jwt from the headers
+//   // STEP 2. use the jsonwebtoken library to verify that it is a valid jwt
+//   // jsonwebtoken dock - https://www.npmjs.com/package/jsonwebtoken
+//   // STEP 3: to prove that everything is working correctly, send the opened jwt back to the front-end
 
-})
+// })
+
+
+// class book{
+//   constructor(title,description,status,email){
+// this.title=title;
+// this.description=description;
+// this.status=status;
+// this.email=email;
+//   }
+
+// }
 
 App.listen(PORT, () => console.log(`listening on ${PORT}`));
